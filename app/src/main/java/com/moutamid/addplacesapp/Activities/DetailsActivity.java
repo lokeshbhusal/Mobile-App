cc57d4e37fac81b1d9d9c5384c4e8939c24ffbb2
lokeshbhusal <bhusallokesh19@gmail.com>
2024-05-19 14:08:37 +1000

    folder structure

---
+++ app/src/main/java/com/moutamid/addplacesapp/Activities/DetailsActivity.java
@@ -0,0 +1,7 @@
+package com.moutamid.addplacesapp.Activities;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class DetailsActivity extends AppCompatActivity{
+
+}
---
+++ app/src/main/java/com/moutamid/addplacesapp/Activities/HomePage.java
@@ -0,0 +1,8 @@
+
+package com.moutamid.addplacesapp.Activities;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class HomePage extends AppCompatActivity{
+
+}
